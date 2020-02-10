3a6e16c17ce7c48638a12585503080ac1ce30ed7
Roland Walker <walker@pobox.com>
2020-02-10 09:04:01 -0500

    remove defunct local variable setting

--- simpleclip.el
+++ simpleclip.el
@@ -512,7 +512,6 @@ is 'toggle."
 ;;
 ;; Local Variables:
 ;; indent-tabs-mode: nil
-;; mangle-whitespace: t
 ;; require-final-newline: t
 ;; coding: utf-8
 ;; byte-compile-warnings: (not cl-functions redefine)
